a50f1f6f063e37d40f7fbefea8847dc8afa7e431
ciyucapa <cindycaceres134@gmail.com>
2022-02-01 18:20:54 -0500

    add context cart

--- src/components/Starts.js
+++ src/components/Starts.js
@@ -6,11 +6,11 @@ const Starts = () => {
     const [stars, setStars] = useState(5)
     return (
         <SelectStars selected={stars}>
-            <span onClick={() => setStars(1)}>★</span>
-            <span onClick={() => setStars(2)}>★</span>
-            <span onClick={() => setStars(3)}>★</span>
-            <span onClick={() => setStars(4)}>★</span>
-            <span onClick={() => setStars(5)}>★</span>
+            <span role="button" tabIndex="0" onClick={() => setStars(1)} onKeyDown={() => setStars(1)}>★</span>
+            <span role="button" tabIndex="0" onClick={() => setStars(2)} onKeyDown={() => setStars(2)}>★</span>
+            <span role="button" tabIndex="0" onClick={() => setStars(3)} onKeyDown={() => setStars(3)}>★</span>
+            <span role="button" tabIndex="0" onClick={() => setStars(4)} onKeyDown={() => setStars(4)}>★</span>
+            <span role="button" tabIndex="0" onClick={() => setStars(5)} onKeyDown={() => setStars(5)}>★</span>
         </SelectStars>
     )
 }
